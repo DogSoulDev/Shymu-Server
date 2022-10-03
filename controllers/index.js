1ea76edeea01347870a9746fc4544562fa975d0c
Javier Fernández <dogsouldev@gmail.com>
2022-10-03 16:44:51 +0200

    add genreController example

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,14 @@
-const userController = require('./usersController');
-
+//!Todos los Controllers en un mismo archivo.
+const userController = require("./userController");
+const trackController = require("./trackController");
+const genreController = require("./genreController");
+const playlistController = require("./playlistController");
+const searchController = require("./searchController");
 
 module.exports = {
-  userController,
+	userController,
+	trackController,
+	genreController,
+	playlistController,
+	searchController,
 };
