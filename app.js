b1764047d95a1827036c42dfa04da99717dbc0e0
Javier Fernández <dogsouldev@gmail.com>
2022-10-06 17:09:04 +0200

    test server before create firebase auth

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//* Importar e inicializar express asociarlo a app para poder interactuar luego.
 const express = require("express");
 const app = express();
-
-//* Importar cors:
 const cors = require('cors');
-//* Habilitar todas las peticiones cors:
+
+
+
 app.use(cors());
-//* Para trabajar con json se carga el modulo json de express: (Para leer de objeto a json y viceversa).
 app.use(express.json());
-app.use(express.urlencoded({extended: true})); //*Necesaria la codificación
+app.use(express.urlencoded({extended: true}));
 
 
-//* Exportar app para index.js:
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 //*Primer paso en verde.
 //?Puntos a tener en cuenta.
 //!Importante.
-
-//*Importar mongoose.
-const mongoose = require("mongoose");
-//*Importamos dotenv y lo llamamos(para que lea lo que tenemos en el archivo .env)."
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 dotenv.config();
+const app = require('./app');
 
-//*Importamos app y le agregamos el framework de express.
-const app = require("./app");
 
-//*Conecta Nodejs con Mongodb usando mongoose(javascript library).
 mongoose.connect(`${process.env.DB_MONGODB}`, (err, req) => {
-	try {
-		if (err) {
-			throw err;
-		} else {
-			console.log("Connection to Database OK!");
-		}
-	} catch (error) {
-		console.error("Error connecting to Database!");
-	}
+  try {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Connection to Database OK!');
+    }
+  } catch (error) {
+    console.error('Error connecting to Database!');
+  }
 });
 
-
 app.listen(`${process.env.DB_PORT}`, (req, res) => {
-	console.log("Server runs OK");
+  console.log('Server runs OK');
 });
